ddf79c610944fae33e5c0ed8e9f530a0de6f66c1
Caleb Odinga <calemasanga@gmail.com>
2021-04-22 09:40:37 +0300

    adds construct for creating user and address objects

--- scripts.js
+++ scripts.js
@@ -1,20 +1,10 @@
 //sample user object
-var town = {
-  Name:'Akula'
-}
-var city = {
-  Name:'Nairofi'
-}
-var county = {
-  Name:'Nairofe'
-}
 var userSample= {
   First_Name: 'Johnte',
   Last_Name: 'Madoe',
   Phone_Number: '0702846994',
-  Address: [town, city, county]
+  Address: []
 }
-console.log(userSample.Address[0])
 //business Logic
 //user object
 function UserContact(firstName, lastName, phoneNumber){
@@ -23,6 +13,16 @@ function UserContact(firstName, lastName, phoneNumber){
   this.Phone_Number = phoneNumber,
   this.Address = []
 }
+//user Address
+function UserAddress(town, city, county){
+  this.Town = town,
+  this.City = city,
+  this.County = county
+}
+
 //user Logic
+
 var userSample2 = new UserContact('Magarita','Apondi', '0756789844');
-console.log(userSample2)
+var userSample2Address = new UserAddress('Pemba','Malindi','Mombasa');
+userSample2.Address.push(userSample2Address)
+console.log(userSample2.Address)
